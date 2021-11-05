a44e304095ebda671d3cb2895e43a231d25a4f21
Djallil Elkebir <elkebir.djallil@icloud.com>
2021-11-05 15:14:19 -0400

    Initial Commit

---
+++ DNAStrandAPP/DNAStrandAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  DNAStrandAPPApp.swift
+//  DNAStrandAPP
+//
+//  Created by Djallil Elkebir on 2021-11-05.
+//
+
+import SwiftUI
+
+@main
+struct DNAStrandAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
